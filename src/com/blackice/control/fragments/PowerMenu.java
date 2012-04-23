c7e8bb3f1d9cf6e85c11ff521b039ad675a2f015
travp624 <stygianstudiosdevs@gmail.com>
2012-04-23 03:11:26 -0500

    ability to remove Profile chooser from power menu.
    Signed-off-by: travp624 <stygianstudiosdevs@gmail.com>

--- src/com/blackice/control/fragments/PowerMenu.java
+++ src/com/blackice/control/fragments/PowerMenu.java
@@ -13,18 +13,17 @@ import com.blackice.control.BlackICEPreferenceFragment;
 
 public class PowerMenu extends BlackICEPreferenceFragment {
 
-    private static final String PREF_POWER_SAVER = "show_power_saver";
     private static final String PREF_SCREENSHOT = "show_screenshot";
     private static final String PREF_TORCH_TOGGLE = "show_torch_toggle";
     private static final String PREF_NAVBAR_HIDE = "show_navbar_hide";
     private static final String PREF_AIRPLANE_TOGGLE = "show_airplane_toggle";
- 
+    private static final String PREF_SHOW_PROFILE_CHOOSER = "show_profile_chooser";
 
-    CheckBoxPreference mShowPowerSaver;
     CheckBoxPreference mShowScreenShot;
     CheckBoxPreference mShowTorchToggle;
     CheckBoxPreference mShowAirplaneToggle;
     CheckBoxPreference mShowNavBarHide;
+    CheckBoxPreference mShowProfileChooser;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,18 +31,6 @@ public class PowerMenu extends BlackICEPreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.prefs_powermenu);
-
-        mShowPowerSaver = (CheckBoxPreference) findPreference(PREF_POWER_SAVER);
-        int powerSaverVal = 0;
-        try {
-            powerSaverVal = Settings.Secure.getInt(getActivity()
-                    .getContentResolver(), Settings.Secure.POWER_SAVER_MODE);
-        } catch (SettingNotFoundException e) {
-            mShowPowerSaver.setEnabled(false);
-            mShowPowerSaver
-                    .setSummary("You need to enable power saver before you can see it in the power menu.");
-        }
-        mShowPowerSaver.setChecked(powerSaverVal != 0);
 
         mShowScreenShot = (CheckBoxPreference) findPreference(PREF_SCREENSHOT);
         mShowScreenShot.setChecked(Settings.System.getInt(getActivity()
@@ -61,6 +48,14 @@ public class PowerMenu extends BlackICEPreferenceFragment {
         mShowNavBarHide.setChecked(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE, 0) == 1);
 
+        mShowProfileChooser = (CheckBoxPreference) findPreference(PREF_SHOW_PROFILE_CHOOSER);
+        mShowProfileChooser.setChecked(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_PROFILE_CHOOSER, 0) == 1);
+
+        if(!hasTorch) {
+            getPreferenceScreen().removePreference(mShowTorchToggle);
+        }
+
         // I took this code out to allow Tablets the ability to Hide Navbar.
         // Note going to completely delete it incase an issue arises and needs to
         // be re-implemented - Z
@@ -72,12 +67,7 @@ public class PowerMenu extends BlackICEPreferenceFragment {
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
             Preference preference) {
-        if (preference == mShowPowerSaver) {
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.POWER_DIALOG_SHOW_POWER_SAVER,
-                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
-            return true;
-        } else if (preference == mShowScreenShot) {
+        if (preference == mShowScreenShot) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWER_DIALOG_SHOW_SCREENSHOT,
                     ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
@@ -97,8 +87,12 @@ public class PowerMenu extends BlackICEPreferenceFragment {
                     Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE,
                     ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
             return true;
+        } else if (preference == mShowProfileChooser) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_DIALOG_SHOW_PROFILE_CHOOSER,
+                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
+            return true;
         }
-
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 }
