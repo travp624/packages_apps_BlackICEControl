4e2d157520e1b99e91937def3af8d2844ea2ea5c
travp624 <stygianstudiosdevs@gmail.com>
2012-04-30 13:24:16 -0500

    fix custom weather app not set summary.

--- src/com/blackice/control/fragments/Weather.java
+++ src/com/blackice/control/fragments/Weather.java
@@ -232,9 +232,9 @@ public class Weather extends BlackICEPreferenceFragment implements
     public void shortcutPicked(String uri, String friendlyName, Bitmap bmp, boolean isApplication) {
 
         if (WeatherPrefs.setCustomApp(mContext, uri)) {
-
             mCustomWeatherApp.setSummary(friendlyName);
-
+        } else if (!WeatherPrefs.setCustomApp(mContext, uri)){
+            mCustomWeatherApp.setSummary(R.string.weather_custom_app_summary);
         }
     }
     
